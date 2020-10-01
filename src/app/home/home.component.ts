dc07eea90716405e0fe5f1e207b47796debcc7c9
Ahtazaz Ahmed Khan <49054500+ahmedahtazaz@users.noreply.github.com>
2020-10-01 16:44:09 +0500

    Interval observable.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  intObs: Subscription;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnDestroy(): void {
+    this.intObs.unsubscribe();
   }
+
+  ngOnInit() {
+    this.intObs = interval(1000).subscribe(
+      count => console.log(count)
+    );
+  }
+
+
 
 }
